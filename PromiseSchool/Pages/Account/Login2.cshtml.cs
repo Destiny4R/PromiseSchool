ad92999c76a6c4d59a9fab7f5b374ea35724452e
japheth kor <JAPHETKOR@GMAIL.COM>
2024-11-22 01:05:51 +0100

    Add project files.

---
+++ PromiseSchool/Pages/Account/Login2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PromiseSchool.Pages.Account
+{
+    public class Login2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PromiseSchool/Pages/Account/Login3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PromiseSchool.Pages.Account
+{
+    public class Login3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
